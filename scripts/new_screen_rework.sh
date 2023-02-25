0ca892cc19796741dfb21059ef5bd55916b9397f
TRudenko22 <timothy.rudenkom@gmail.com>
2023-02-24 23:22:57 -0500

    added changes to note for better server interaction

---
+++ scripts/new_screen_rework.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-0 --off --output DP-1 --mode 1920x1080 --pos 1080x0 --rotate right --output HDMI-0 --mode 2560x1080 --pos 2160x0 --rotate normal --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-5 --mode 1920x1080 --pos 0x0 --rotate left
